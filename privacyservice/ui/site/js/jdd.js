277a4135c5e11ed14e339ca4527e176f98a254f0
Monit khatri <81676316+ClGAR@users.noreply.github.com>
2023-01-03 19:33:19 +0530

    Update jdd.js
    tested

--- privacyservice/ui/site/js/jdd.js
+++ privacyservice/ui/site/js/jdd.js
@@ -878,7 +878,6 @@ var jdd = {
     },
 
     getParameterByName: function (name) {
-        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
         var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
             results = regex.exec(location.search);
         return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
